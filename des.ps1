448c0cae1030ba209546c6bc3a296bb403bdaf9b
tingbol <tingobol@hotmail.com>
2024-03-07 16:01:56 -0400

    7zip

--- des.ps1
+++ des.ps1
@@ -1,8 +1,15 @@
 # List of file URLs to download
 $Urls = @(
     "https://download.anydesk.com/AnyDesk.exe",
-    "https://github.com/tingobol/wpi/raw/main/WinRar/winrar-x64-624es.exe"
-)
+    "https://github.com/tingobol/wpi/raw/main/WinRar/winrar-x64-624es.exe",
+	"https://dl.google.com/tag/s/appguid%3D%7B8A69D345-D564-463C-AFF1-A69D9E530F96%7D%26iid%3D%7BD5CE696E-242A-0F7E-D4EF-BC1A4034EFAC%7D%26lang%3Des%26browser%3D3%26usagestats%3D0%26appname%3DGoogle%2520Chrome%26needsadmin%3Dprefers%26ap%3Dx64-stable-statsdef_1%26installdataindex%3Dempty/chrome/install/ChromeStandaloneSetup64.exe",
+	"https://github.com/tingobol/wpi/raw/main/Firefox/Firefox Setup 123.0.1.exe",
+   	"https://www.7-zip.org/a/7z2401-x64.exe",
+	"https://get.videolan.org/vlc/3.0.20/win64/vlc-3.0.20-win64.exe",
+	""
+
+	
+	)
  
 # Destination folder
 $DestinationFolder = "C:\temp\"
